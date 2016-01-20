b9fb46e71f45ceab11dbb3f424902d660a78d0a7
Sneha Gaikwad <snehag28@ccs.neu.edu>
2016-01-20 18:50:27 -0500

    changed server.js to add /

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.get('', function(req, res){
+app.get('/', function(req, res){
   res.send('hello world');
 });
 app.listen(port, ipaddress);
