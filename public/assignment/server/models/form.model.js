7f6254014875beb8cacb437f15bb87ac4a6be563
Sneha Gaikwad <snehag28@ccs.neu.edu>
2016-03-14 17:07:19 -0700

    created mock models and api

--- public/assignment/server/models/form.model.js
+++ public/assignment/server/models/form.model.js
@@ -1,3 +1,60 @@
-/**
- * Created by snehagaikwad on 3/13/16.
- */
+var forms = require("./user.mock.json");
+
+module.exports = function(){
+    var api = {
+        createFormForUser: createFormForUser,
+        findAllFormsForUser: findAllFormsForUser,
+        deleteFormById: deleteFormById,
+        updateFormById: updateFormById
+    }
+
+    return api;
+
+    function createFormForUser(userId, form) {
+        var _id = (new Date).getTime();
+        form._id = _id;
+        form.userId = userId;
+        forms.push(form);
+        return form;
+    }
+
+    function findAllFormsForUser(userId) {
+        var formsForUser = [];
+        for (var index in forms) {
+            var form = forms[index];
+            if (form.userId == userId) {
+                formsForUser.push(form);
+            }
+        }
+        return formsForUser;
+    }
+
+    //function that returns form based on the ID
+    function findFormById(formId) {
+        for (var index in forms) {
+            var form = forms[index];
+            if (formId == form._id) {
+                return form;
+            }
+        }
+        return null;
+    }
+
+    function deleteFormById(formId) {
+        var form = findFormById(formId);
+        if (form != null) {
+            var index = forms.indexOf(form);
+            forms.splice(index, 1);
+        }
+        return forms;
+    }
+
+    function updateFormById(formId, newForm) {
+        var form = findFormById(formId);
+        if (form != null) {
+            form.title = newForm.title;
+        }
+        return form;
+    }
+}
+
--- public/assignment/server/models/user.model.js
+++ public/assignment/server/models/user.model.js
@@ -1,3 +1,73 @@
-/**
- * Created by snehagaikwad on 3/13/16.
- */
+var users = require("./user.mock.json");
+
+module.exports = function() {
+    var api = {
+        findUserByCredentials: findUserByCredentials,
+        findAllUsers: findAllUsers,
+        createUser: createUser,
+        deleteUserById: deleteUserById,
+        updateUser: updateUser
+    };
+    return api;
+
+    function findUserByCredentials(username, password) {
+        for (var index in users) {
+            if (users[index].username == username && users[index].password == password) {
+                return users[index];
+            }
+        }
+        return null;
+    }
+
+    function findAllUsers(callback) {
+        return users;
+    }
+
+    function createUser(user) {
+        var _id = (new Date).getTime();
+        var newUser = {
+            "_id": _id,
+            "username": user.username,
+            "password": user.password,
+            "email" : user.email
+        };
+        //Adds the new user to local array of users
+        users.push(newUser);
+
+        return users;
+    }
+
+    function deleteUserById(userId) {
+        userId = parseInt(userId);
+        var user = findUserById(userId);
+        if(user != null){
+            var index = users.indexOf(user);
+            users.splice(index, 1);
+        }
+        return users;
+    }
+
+    function findUserById(userId) {
+        userId = parseInt(userId);
+        for(var i in users) {
+            if(users[i].id === userId) {
+                return users[i];
+            }
+        }
+        return null;
+    }
+
+    function updateUser(userId, newUser) {
+        var user = findUserById(userId);
+
+        if(user != null){
+            user.firstName = newUser.firstName;
+            user.lastName = newUser.lastName;
+            user.username = newUser.username;
+            user.password = newUser.password;
+            user.roles = newUser.roles;
+            user.email = newUser.email;
+        }
+       return user;
+    }
+};
