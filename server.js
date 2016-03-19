fc0cc27ddcfe07c29f6a105e78f94e3ecde0dd60
Sneha Gaikwad <snehag28@ccs.neu.edu>
2016-03-18 23:15:32 -0400

    server changed

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 #!/bin/env node
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public'));
+var bodyParser    = require('body-parser');
+var multer        = require('multer');
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+app.use(express.static(__dirname + '/public'));
+
+require("./public/assignment/server/app.js")(app);
+require("./public/assignment/server/services/user.service.server.js")(app);
+require("./public/assignment/server/services/form.service.server.js")(app);
+require("./public/assignment/server/services/field.service.server.js")(app);
+
+
 app.listen(port, ipaddress);
